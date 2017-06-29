f50f483a501b3c62adeb44049ae284677a0a7737
Yuya Sato <yuyas203@gmail.com>
2017-06-29 19:26:28 +0900

    comment機能を作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,17 @@
 class Comment < ApplicationRecord
+    def category_color
+        if self.category==0
+            return "black"
+        elsif self.category==1
+            return "red"
+        end
+    end
+
+    def category_icon
+        if self.category==0
+            return "fa-times"
+        elsif self.category==1
+            return "fa-circle-o"
+        end
+    end
 end
